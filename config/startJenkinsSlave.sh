ae270972d8bbaa08ff7ed01d479d53039afc8b3f
ototadana <ototadana@gmail.com>
2015-07-30 19:36:35 +0900

    JenkinsスレーブがProxy経由でマスターに接続しようとする (xpfriend/pocci#2)

--- config/startJenkinsSlave.sh
+++ config/startJenkinsSlave.sh
@@ -5,4 +5,5 @@ if [ ! -f slave.jar ]; then
     wget ${JENKINS_URL}/jnlpJars/slave.jar
 fi
 
+unset http_proxy
 java -jar slave.jar -jnlpUrl ${JENKINS_URL}/computer/${NODE_NAME}/slave-agent.jnlp ${JENKINS_SLAVE_SECRET}
